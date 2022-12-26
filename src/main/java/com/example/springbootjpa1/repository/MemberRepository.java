3be6d89c98f129cfe5ef0ce6f8cb2af9500abd2c
Universenn <dnwndhks98@gmail.com>
2022-12-26 15:03:08 +0900

    MemberRepository 구현

---
+++ src/main/java/com/example/springbootjpa1/repository/MemberRepository.java
@@ -0,0 +1,26 @@
+package com.example.springbootjpa1.repository;
+
+import com.example.springbootjpa1.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    // EntityManager 주입
+    private EntityManager em;
+
+    public Long save(Member member) {
+        // 저장
+        em.persist(member);
+        // 아이디 값 리턴
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
